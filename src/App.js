a9361a9c326889ab0f82d3644c34e2362f9c4c64
박예진 <qkrdpwls2002>
2025-01-22 23:24:10 +0900

    ✨ Feat : 회원 정보 수정 API 구현

--- src/App.js
+++ src/App.js
@@ -1,36 +1,46 @@
+// App.js
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom"; // Switch 추가
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 import HomePage from "./pages/home/Home";
 import LoginPage from "./pages/auth/Login";
-import PaymentPage from "./pages/payment/Payment";
 import SignUpPage from "./pages/auth/SignUp";
-import LogoutPage from "./pages/auth/Logout";
-import Header from "./components/header/Header"; // Header 컴포넌트 경로
+import CommunityPage from "./pages/community/CommunityMain";
+import NewsPage from "./pages/news/NewsMain";
+import MyPage from "./pages/mypage/profile/MyPage";
+import UserUpdatePage from "./pages/mypage/profile/UserUpdate";
+import Header from "./components/header/Header";
+import PaymentPage from "./pages/payment/Payment";
 
-//로그인 상태에서만 /payment 경로에 접근 가능하도록 보호- 추후 분리 고려해보기
-// const PrivateRoute = ({ component: Component, ...rest }) => {
-//   const isAuthenticated = !!localStorage.getItem("jwtToken"); // 로그인 여부 확인
-//   return (
-//     <Route
-//       {...rest}
-//       render={(props) =>
-//         isAuthenticated ? <Component {...props} /> : <Redirect to="/login" />
-//       }
-//     />
-//   );
-// };
+// 로그인이 필요한 페이지를 위한 Private Route
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  const isAuthenticated = !!localStorage.getItem("jwtToken");
+  return (
+      <Route
+          {...rest}
+          render={(props) =>
+              isAuthenticated ? <Component {...props} /> : <Redirect to="/login" />
+          }
+      />
+  );
+};
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/login" component={LoginPage} />
-        <Route path="/logout" component={LogoutPage} />
-        <Route path="/payment" component={PaymentPage} />
-        <Route path="/signup" component={SignUpPage} />
-      </Switch>
-    </Router>
+      <Router>
+        <Header /> {/* 모든 페이지에 헤더 추가 */}
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/login" component={LoginPage} />
+          <Route path="/signup" component={SignUpPage} />
+          <Route path="/community" component={CommunityPage} />
+          <Route path="/news" component={NewsPage} />
+          <Route path="/payment" component={PaymentPage} />
+
+          {/* 로그인 필요한 페이지는 PrivateRoute로 보호 */}
+          <PrivateRoute path="/mypage" component={MyPage} />
+          <PrivateRoute path="/user/update" component={UserUpdatePage} />
+        </Switch>
+      </Router>
   );
 }
 
