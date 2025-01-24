84880b6b9fe9a6971f88da7ff3eaa6ea640d7fde
박예진 <qkrdpwls2002>
2025-01-25 00:41:45 +0900

    🐛 Fix : 어드민 사이드바 공지사항 관리 연결

--- src/pages/admin/AdminRoutes.js
+++ src/pages/admin/AdminRoutes.js
@@ -5,6 +5,7 @@ import MentorRequestList from './mentor-request/MentorRequest'
 import MentorRequestDetail from './mentor-request/MentorRequestDetail';
 import MentorList from "./mentor-list/MentorList";
 import MentorDetail from './mentor-list/MentorDetail'
+import CommunityNotice from "../community/CommunityNotice";
 
 const AdminRoutes = () => {
   return (
@@ -14,9 +15,7 @@ const AdminRoutes = () => {
           <Route exact path="/admin/mentor-request/:mentorId" component={MentorRequestDetail} />
           <Route exact path="/admin/mentor-list" component={MentorList} />
           <Route exact path="/admin/mentor-list/:mentorId" component={MentorDetail} />
-          <Route exact path="/admin/notice">
-            <div>공지사항 관리</div>
-          </Route>
+          <Route exact path="/admin/notice" component={CommunityNotice} />
           <Route exact path="/admin/settlement">
             <div>정산 내역 관리</div>
           </Route>
