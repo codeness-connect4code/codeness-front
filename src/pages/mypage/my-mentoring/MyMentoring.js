4dcd58e4ccbe4c3942ca716e5dac84fb6b86c554
parkmookeun <sohappynow12@naver.com>
2025-01-25 19:11:39 +0900

    ✨ Feat : 내 멘토링 페이지
    멘토 -> 내 공고(삭제 버튼) / 멘티 -> 내가 받은 멘토링(삭제 버튼 x)

---
+++ src/pages/mypage/my-mentoring/MyMentoring.js
@@ -0,0 +1,116 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './MyMentoring.css'
+import TrashButton from '../../../components/button/TrashButton';
+
+const MyMentoring = () => {
+
+    //내가 여태 받은 or 했던 멘토링 공고에 관한 내역을 조회한다.
+    //멘토 & 멘티 : 멘토링 공고 내역 조회
+    //멘토인 경우에 삭제 아이콘 표시 -> 삭제 시 다시 리다이렉션
+
+    //내가 멘토인지 멘토인지 구분
+    let userData = null;
+
+    const[myData, setMyData] = useState(null);
+
+    const token = localStorage.getItem('jwtToken');
+
+    const parseJwt = (token) => {
+
+        const payload = JSON.parse(atob(token.split('.')[1]));
+        return payload.role;
+
+        };
+
+    if (token) {
+       userData = parseJwt(token);        
+    }
+    
+useEffect(() => {
+    if (userData === 'MENTEE'){
+        const fetchData = async() => {
+            try{
+                const response = await axios.get('http://localhost:8080/mentees/mentoring',{
+                    headers: {
+                        'Authorization': `Bearer ${token}`
+                    }
+                    });
+                    setMyData(response.data.data.content);
+                }catch(error){
+                    if(!axios.isCancel(error)){
+                        console.error('내 멘토링 데이터 가져오기 실패:', error);
+                    }
+                }
+            };
+            fetchData();
+        }
+        else if(userData === 'MENTOR'){
+            const fetchData = async() => {
+                try{
+                    const response = await axios.get('http://localhost:8080/mentors/mentoring',{
+                        headers: {
+                            'Authorization': `Bearer ${token}`
+                        }
+                        });
+                        setMyData(response.data.data);
+                    }catch(error){
+                        if(!axios.isCancel(error)){
+                            console.error('내 멘토링 데이터 가져오기 실패:', error);
+                        }
+                    }
+                };
+                fetchData();
+        }
+    },[userData,token]);
+
+    // API 호출로 리뷰 삭제
+    const deleteItem = async (id) => {
+        try {
+            const response = await axios({
+                method: 'patch',
+                url: `/mentoring/${id}`,
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                    'Content-Type': 'application/json'
+                }
+            });
+            console.log('삭제 성공:', response);
+            window.location.reload();
+        } catch (error) {
+        console.error('삭제 실패:', error);
+        }
+    };
+
+    return(
+        <div className="grid-container">
+            {myData && (Array.isArray(myData) ? (
+                myData.map((item, index) => (
+                    <div key={index} className="grid-item">
+                        <p>[{item.title}]</p>
+                        <p>분야: {item.field}</p>
+                        <p>경력: {item.career}</p>
+                        <p>닉네임: {item.userNickname}</p>
+                        <p>별점: <span>★</span> {item.starRating}</p>
+                    </div>
+                ))
+            ) : (
+                <div className='grid-item'>
+                    <p>[{myData.title}]</p>
+                    <p>분야: {myData.field}</p>
+                    <p>경력: {myData.career}</p>
+                    <p>닉네임: {myData.userNickname}</p>
+                    <p>별점: <span>★</span> {myData.starRating}</p>
+                    <div style={{ position: 'absolute', bottom: '10px', right: '10px' }}>
+                    <TrashButton 
+                                onDelete={(id) => deleteItem(id)}
+                                id={myData.mentoringPostId}
+                                url='/mypage/profile'/>
+                    </div>
+                </div>
+            ))}
+            </div>
+    )
+}
+
+export default MyMentoring;
