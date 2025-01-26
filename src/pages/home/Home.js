a98bec45c07745db63921b3759bd82eb86d51644
박예진 <qkrdpwls2002>
2025-01-26 23:32:43 +0900

    🐛 Fix : 소셜 로그인 리다이렉트 수정

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useHistory, useLocation } from 'react-router-dom'; // v5에서 useHistory 사용
+import axios from 'axios';
 import MentorRecommendation from './MentorRecommendation';
 import MainNewsList from '../news/MainNews';
 import CompanyInfo from './CompanyInfo';
 import MainPosts from './MainPosts';
 
 function Home() {
-    return (
+  const history = useHistory();
+  const location = useLocation();
+
+  useEffect(() => {
+    // URL에서 토큰 파라미터 확인 (소셜 로그인 리다이렉트)
+    const params = new URLSearchParams(location.search);
+    const token = params.get("token");
+
+    if (token) {
+      localStorage.setItem("jwtToken", token);
+      axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+      // 로그인 후 루트('/')로 리디렉션
+      history.replace("/"); // 페이지를 루트로 리디렉션
+    }
+
+    const savedToken = localStorage.getItem("jwtToken");
+    // 추가적인 로직 처리
+
+  }, [location, history]); // 의존성 배열에 location, history 추가
+
+  return (
       <div>
         <MentorRecommendation />
         <div style={{
@@ -28,7 +50,7 @@ function Home() {
           </div>
         </div>
       </div>
-    );
+  );
 }
 
 export default Home;
